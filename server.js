94ffb3f5d131b79eaf42ef24175a6f2c76c4bf8e
web-collab <tanmoy587@gmail.com>
2024-04-30 14:16:05 +0530

    Server & Mongodb Setup

--- server.js
+++ server.js
@@ -3,23 +3,23 @@ import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
-// import connectDB from "./config/db.js";
+import connectDB from "./config/db.js";
 // import authRoutes from "./routes/authRoute.js";
 // import cors from "cors";
 
 //configure env
 dotenv.config();
 
-// //databse config
-// connectDB();
+//databse config
+connectDB();
 
 //rest object
 const app = express();
 
-// //middelwares
+//middelwares
 // app.use(cors());
-// app.use(express.json());
-// app.use(morgan("dev"));
+app.use(express.json());
+app.use(morgan("dev"));
 
 // //routes
 // app.use("/api/v1/auth", authRoutes);
@@ -35,6 +35,7 @@ const PORT = process.env.PORT || 8080;
 //run listen
 app.listen(PORT, () => {
   console.log(
-    `Server Running on mode on port ${PORT}`
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
   );
 });
