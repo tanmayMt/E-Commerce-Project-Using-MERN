fc8e7d3db0bc38722392d2113a55f4c47a55af54
web-collab <tanmoy587@gmail.com>
2024-04-30 16:18:04 +0530

    models & hashing password And register api

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
-// import authRoutes from "./routes/authRoute.js";
+import authRoutes from "./routes/authRoute.js";
 // import cors from "cors";
 
 //configure env
@@ -21,8 +21,8 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 
-// //routes
-// app.use("/api/v1/auth", authRoutes);
+//routes
+app.use("/api/v1/auth", authRoutes);
 
 //rest api
 app.get("/", (req, res) => {
