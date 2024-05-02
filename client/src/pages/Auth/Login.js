6c0e7afcec0e19d6c2f9dc6b5ff418916e5867c9
web-collab <tanmoy587@gmail.com>
2024-05-02 20:03:47 +0530

    context api 3:04:36

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 import Layout from "./../../components/Layout/Layout";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import toast from "react-hot-toast";
 import "../../styles/AuthStyles.css";
-
+import { useAuth } from "../../context/auth";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [auth, setAuth] = useAuth();
 
   const navigate = useNavigate();
+  const location = useLocation();
 
   // form function
   const handleSubmit = async (e) => {
@@ -21,6 +23,13 @@ const Login = () => {
       });
       if (res && res.data.success) {
         toast.success(res.data && res.data.message);
+        setAuth({
+          ...auth,
+          user: res.data.user,
+          token: res.data.token,
+        });
+        localStorage.setItem("auth", JSON.stringify(res.data));
+        //navigate(location.state || "/");
         navigate("/");
       } else {
         toast.error(res.data.message);
@@ -32,13 +41,14 @@ const Login = () => {
   };
   return (
     <Layout title="Register - Ecommer App">
-      <div className="form-container ">
+      <div className="form-container " style={{ minHeight: "90vh" }}>
         <form onSubmit={handleSubmit}>
           <h4 className="title">LOGIN FORM</h4>
 
           <div className="mb-3">
             <input
               type="email"
+              autoFocus
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               className="form-control"
@@ -57,6 +67,17 @@ const Login = () => {
               placeholder="Enter Your Password"
               required
             />
+          </div>
+          <div className="mb-3">
+            <button
+              type="button"
+              className="btn forgot-btn"
+              onClick={() => {
+                navigate("/forgot-password");
+              }}
+            >
+              Forgot Password
+            </button>
           </div>
 
           <button type="submit" className="btn btn-primary">
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+// import { Checkbox, Radio } from "antd";
+// import { Prices } from "../components/Prices";
+import { useCart } from "../context/cart";
+import axios from "axios";
+import toast from "react-hot-toast";
 import Layout from "./../components/Layout/Layout";
+// import { AiOutlineReload } from "react-icons/ai";
+import "../styles/Homepage.css";
+import {useAuth} from "../context/auth";
 
 const HomePage = () => {
+  const [auth,setAuth] = useAuth();
+  // const navigate = useNavigate();
+  // const [cart, setCart] = useCart();
+  // const [products, setProducts] = useState([]);
+  // const [categories, setCategories] = useState([]);
+  // const [checked, setChecked] = useState([]);
+  // const [radio, setRadio] = useState([]);
+  // const [total, setTotal] = useState(0);
+  // const [page, setPage] = useState(1);
+  // const [loading, setLoading] = useState(false);
   return (
-    <Layout title={"Best offers "}>
+     <Layout title={"All Products - Best offers "}>
+        {/* banner image */}
+        <img
+          src="/images/banner.png"
+          className="banner-img"
+          alt="bannerimage"
+          width={"100%"}
+        />
+
       <h1>HomePage</h1>
+      <pre>{JSON.stringify(auth, null,4)}</pre>
     </Layout>
   );
 };
