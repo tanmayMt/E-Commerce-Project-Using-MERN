ab2a814095a932fce6bc34d382ce78892a139aa5
web-collab <tanmoy587@gmail.com>
2024-05-03 18:55:27 +0530

    Create Product -> 6:26:10

--- client/src/pages/Admin/CreateProduct.js
+++ client/src/pages/Admin/CreateProduct.js
@@ -3,9 +3,9 @@ import Layout from "./../../components/Layout/Layout";
 import AdminMenu from "./../../components/Layout/AdminMenu";
 import toast from "react-hot-toast";
 import axios from "axios";
-// import { Select } from "antd";
+import { Select } from "antd";
 import { useNavigate } from "react-router-dom";
-// const { Option } = Select;
+const { Option } = Select;
 
 const CreateProduct = () => {
   const navigate = useNavigate();
@@ -36,31 +36,31 @@ const CreateProduct = () => {
   }, []);
 
   //create product function
-  // const handleCreate = async (e) => {
-  //   e.preventDefault();
-  //   try {
-  //     const productData = new FormData();
-  //     productData.append("name", name);
-  //     productData.append("description", description);
-  //     productData.append("price", price);
-  //     productData.append("quantity", quantity);
-  //     productData.append("photo", photo);
-  //     productData.append("category", category);
-  //     const { data } = axios.post(
-  //       "/api/v1/product/create-product",
-  //       productData
-  //     );
-  //     if (data?.success) {
-  //       toast.error(data?.message);
-  //     } else {
-  //       toast.success("Product Created Successfully");
-  //       navigate("/dashboard/admin/products");
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //     toast.error("something went wrong");
-  //   }
-  // };
+  const handleCreate = async (e) => {
+    e.preventDefault();
+    try {
+      const productData = new FormData();
+      productData.append("name", name);
+      productData.append("description", description);
+      productData.append("price", price);
+      productData.append("quantity", quantity);
+      productData.append("photo", photo);
+      productData.append("category", category);
+      const { data } = axios.post(
+        "/api/v1/product/create-product",
+        productData
+      );
+      if (data?.success) {
+        toast.error(data?.message);
+      } else {
+        toast.success("Product Created Successfully");
+        navigate("/dashboard/admin/products");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("something went wrong");
+    }
+  };
 
   return (
     <Layout title={"Dashboard - Create Product"}>
@@ -72,7 +72,7 @@ const CreateProduct = () => {
           <div className="col-md-9">
             <h1>Create Product</h1>
             <div className="m-1 w-75">
-              {/* <Select
+              <Select
                 bordered={false}
                 placeholder="Select a category"
                 size="large"
@@ -87,7 +87,7 @@ const CreateProduct = () => {
                     {c.name}
                   </Option>
                 ))}
-              </Select> */}
+              </Select>
               <div className="mb-3">
                 <label className="btn btn-outline-secondary col-md-12">
                   {photo ? photo.name : "Upload Photo"}
@@ -150,7 +150,7 @@ const CreateProduct = () => {
                 />
               </div>
               <div className="mb-3">
-                {/* <Select
+                <Select
                   bordered={false}
                   placeholder="Select Shipping "
                   size="large"
@@ -162,11 +162,11 @@ const CreateProduct = () => {
                 >
                   <Option value="0">No</Option>
                   <Option value="1">Yes</Option>
-                </Select> */}
+                </Select> 
               </div>
               <div className="mb-3">
                 <button className="btn btn-primary" 
-                //onClick={handleCreate}
+                onClick={handleCreate}
                 >
                   CREATE PRODUCT
                 </button>
