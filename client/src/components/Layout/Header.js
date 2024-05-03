522b2fc817c3531db6a12432ae430df9851884cb
web-collab <tanmoy587@gmail.com>
2024-05-03 09:12:10 +0530

    3:55:30

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -56,11 +56,33 @@ const Header = () => {
                 </NavLink>
               </li>
               </>):(<>
-               <li className="nav-item">
-                <NavLink to="/login" onClick={handleLogout} className="nav-link">
-                  Logout
-                </NavLink>
-              </li>
+                  <li className="nav-item dropdown">
+                    <NavLink
+                      className="nav-link dropdown-toggle"
+                      href="#"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      {auth?.user?.name}
+                    </NavLink>
+                    <ul className="dropdown-menu">
+                      <li>
+                        <NavLink to="/dashboard" className="dropdown-item">
+                          Dashboard
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink
+                          onClick={handleLogout}
+                          to="/login"
+                          className="dropdown-item"
+                        >
+                          Logout
+                        </NavLink>
+                      </li>
+                    </ul>
+                  </li>
               </>)}
 
               <li className="nav-item">
